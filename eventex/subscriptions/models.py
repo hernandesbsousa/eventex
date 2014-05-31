6803dacb0ee38381696532a4623df1e6536c0da8
Hernandes B. de Sousa <hernandes.sousa@gmail.com>
2014-05-31 20:16:57 -0300

    Add translation for model fields

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100)
-    cpf = models.CharField(max_length=11, unique=True)
-    email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(_('nome'), max_length=100)
+    cpf = models.CharField(_('CPF'), max_length=11, unique=True)
+    email = models.EmailField(_('email'), unique=True)
+    phone = models.CharField(_('telefone'), max_length=20, blank=True)
+    created_at = models.DateTimeField(_('criado em'), auto_now_add=True)
 
     def __unicode__(self):
         return self.name
